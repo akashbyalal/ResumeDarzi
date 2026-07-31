8b932134c6825ddc930606d63b57385007b0669b
Akash Byalal <akashbyalal02@gmail.com>
2026-07-31 22:05:00 +0530

    Fix frontend build issues

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "./features/auth/pages/Login.jsx";
-import Register from "./features/auth/pages/register.jsx";
+import Register from "./features/auth/pages/Register.jsx";
 import Protected from "./features/auth/components/Protected.jsx";
 import Home from "./features/interview/pages/Home.jsx"
 import Interview from "./features/interview/pages/Interview.jsx";
